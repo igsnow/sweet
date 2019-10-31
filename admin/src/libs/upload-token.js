fde966cc4f707630835078f0be8e080005acc50d
igsnow <zzy716810918@gmail.com>
2019-10-31 17:06:26 +0800

    init

---
+++ admin/src/libs/upload-token.js
@@ -0,0 +1,27 @@
+const qiniu = require('qiniu')
+
+// 自己可以到"www.qiniu.com" 申请自己的七牛空间
+// 这个账号是我的，不是开放的哦
+const ACCESS_KEY = "usNXW9nMId251rs0EBxhiA3CqFSs6NgWWfeTDhJp";
+const SECRET_KEY = "zH6gubVQcpXTVk0iSPuPelx0jRR-Pit-uUWn4dmB";
+
+export default async function getUploadToken() {
+  return new Promise((resolve, reject) => {
+    let mac = new qiniu.auth.digest.Mac(ACCESS_KEY, SECRET_KEY);
+    let options = {
+      scope: "zzy716810918",
+      expires: 7200
+    };
+    let putPolicy = new qiniu.rs.PutPolicy(options);
+    let uploadToken = putPolicy.uploadToken(mac);
+
+    if (uploadToken) {
+      resolve({
+        token: uploadToken
+      })
+    } else {
+      reject()
+    }
+  })
+}
+
